ccf11c84a9d289aeec98456f48b049273c784620
Alexander Inman <fujiwara06@gmail.com>
2018-10-27 18:56:09 -0400

    First commit

---
+++ Lab9/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Class 1-A. Please choose a student to learn more information about them. (enter a number 1-20): ");
+            string userInput;
+            try
+            {
+                userInput = Console.ReadLine();
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                throw e;
+            }
+            Console.WriteLine($"Student chosen: {userInput}");
+
+            Console.ReadKey();
+        }
+    }
+
+    public enum Student
+    {
+        Sally,
+
+        Daniel,
+
+        Markus,
+
+        Galinda,
+
+        Peter,
+
+        Stephen,
+
+        Tyler,
+
+        Maddie,
+
+        June,
+
+        Roger,
+
+        Frank,
+
+
+    }
+}
