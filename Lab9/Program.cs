89aba111c7d5861e988449026f320bdad96df3c6
Alexander Inman <fujiwara06@gmail.com>
2018-10-28 22:43:27 -0400

    Progress

--- Lab9/Program.cs
+++ Lab9/Program.cs
@@ -8,48 +8,125 @@ namespace Lab9
 {
     class Program
     {
+        static string userInput;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Class 1-A. Please choose a student to learn more information about them. (enter a number 1-20): ");
-            string userInput;
-            try
+            Initialize();
+            string selectedStudent = "";
+            int studentIndex = 0;
+            while (userInput != "")
             {
-                userInput = Console.ReadLine();
+                if (int.TryParse(userInput, out int value))
+                {
+                    studentIndex = value;
+                    try
+                    {
+                        selectedStudent = Students[studentIndex - 1];
+                    }
+                    catch (ArgumentOutOfRangeException e)
+                    {
+                        Console.WriteLine("ID must be between 1 and 20");
+                    }
+
+                    Console.WriteLine($"Student chosen: ({userInput})-{selectedStudent}");
+                }
+
+                break;
             }
-            catch (IndexOutOfRangeException e)
+
+            Console.Write($"What would you like to know about {selectedStudent}? (height or birthdate):");
+            userInput = Console.ReadLine();
+
+            if (userInput == "height")
             {
-                throw e;
+                Console.WriteLine($"{selectedStudent} is {Heights[studentIndex]} tall.");
             }
-            Console.WriteLine($"Student chosen: {userInput}");
+            else if (userInput == "birthdate")
+            {
+                Console.WriteLine($"{selectedStudent} was born on {Birthdates[studentIndex]}.");
+            }
 
             Console.ReadKey();
         }
-    }
 
-    public enum Student
-    {
-        Sally,
+        public static void Initialize()
+        {
+            Console.Write("Welcome to Class 1-A. Please choose a student to learn more information about them. (enter a number 1-20): ");
+            userInput = Console.ReadLine();
 
-        Daniel,
+        }
 
-        Markus,
+        public static List<string> Students = new List<string>
+        {
+            "Aoyama Yuga",
+            "Ashido Mina",
+            "Asui Tsuyu",
+            "Hagakure Toru",
+            "Iida Tenya",
+            "Jiro Kyoka",
+            "Kaminari Denki",
+            "Katsuki Bakugo",
+            "Kirishima Eijiro",
+            "Koda Koji",
+            "Midoriya Izuku",
+            "Mineta Minoru",
+            "Ojiro Mashirao",
+            "Sato Rikido",
+            "Sero Hanta",
+            "Shoji Mezo",
+            "Todoroki Shoto",
+            "Tokoyami Fumikage",
+            "Uraraka Ochaco",
+            "Yaoyorozu Momo"
+        };
 
-        Galinda,
+        public static List<string> Heights = new List<string>
+        {
+            "168cm",
+            "159cm",
+            "150cm",
+            "154cm",
+            "179cm",
+            "154cm",
+            "168cm",
+            "172cm",
+            "170cm",
+            "186cm",
+            "166cm",
+            "108cm",
+            "169cm",
+            "185cm",
+            "177cm",
+            "187cm",
+            "176cm",
+            "158cm",
+            "156cm",
+            "173cm"
+        };
 
-        Peter,
-
-        Stephen,
-
-        Tyler,
-
-        Maddie,
-
-        June,
-
-        Roger,
-
-        Frank,
-
-
+        public static List<string> Birthdates = new List<string>
+        {
+            "30-May",
+            "30-Jul",
+            "12-Feb",
+            "16-Jun",
+            "22-Aug",
+            "1-Aug",
+            "29-Jun",
+            "20-Apr",
+            "16-Oct",
+            "1-Feb",
+            "15-Jul",
+            "8-Oct",
+            "28-May",
+            "19-Jun",
+            "28-Jul",
+            "25-Feb",
+            "11-Jan",
+            "30-Oct",
+            "27-Dec",
+            "23-Sep"
+        };
     }
 }
---
+++ Lab9/Students.cs
@@ -0,0 +1,26 @@
+﻿namespace Lab9
+{
+    public enum Student
+    {
+        AoyamaYuga,
+        AshidoMina,
+        AsuiTsuyu,
+        HagakureToru,
+        IidaTenya,
+        JiroKyoka,
+        KaminariDenki,
+        KatsukiBakugo,
+        KirishimaEijiro,
+        KodaKoji,
+        MidoriyaIzuku,
+        MinetaMinoru,
+        OjiroMashirao,
+        SatoRikido,
+        SeroHanta,
+        ShojiMezo,
+        TodorokiShoto,
+        TokoyamiFumikage,
+        UrarakaOchaco,
+        YaoyorozuMomo
+    }
+}
